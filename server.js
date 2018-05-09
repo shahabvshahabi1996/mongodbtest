c31355b225e525f7ea4b9e5631bd662f9b982c6c
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-05-10 02:03:22 +0330

    added connection method to server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require('./connection');
+
 const app = express();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port , () => {
