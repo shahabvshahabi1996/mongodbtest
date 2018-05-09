ce37f5112b67a0508621b8db92f982229717a394
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-05-10 02:47:29 +0330

    mongodb tutorial part2

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -1,11 +1,25 @@
 const User = require('../model/userModel');
+const md5 = require('md5');
 
 exports.addUser = (req,res) => {
+    new User({
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password,
+        token : req.body.token 
+    }).save((err,result)=>{
+        if(err) throw err;
+        res.json({
+            status : 200,
+            message : 'You have successfully signed Up!',
+            data : result
+        });
+        return ;
+    })
     
 }
 
 exports.findUsers = (req,res) => {
-
     User.find({},(err,result)=>{
         if(err) throw err;
         res.json({
@@ -15,5 +29,14 @@ exports.findUsers = (req,res) => {
         });
         return;
     })
-
 }
+
+//middleware A - > B ==>  A -> C -> B
+
+exports.makeToken = (req,res,next) => {
+    const token = md5([req.body,Date.now()]);
+    req.body.token = token;
+    next();
+    return;
+}
+
--- api/routes.js
+++ api/routes.js
@@ -4,4 +4,9 @@ module.exports = (app) => {
     app.route('/find/all/user').get(
         userController.findUsers
     )
+
+    app.route('/new/user').post(
+        userController.makeToken,
+        userController.addUser
+    )
 }
